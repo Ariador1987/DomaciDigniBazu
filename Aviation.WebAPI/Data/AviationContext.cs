cbe7487bb74155ecaa77070719c13c5eff82370e
Božidar Bralić <bozidar.bralic@gmail.com>
2021-12-16 14:58:50 +0100

    InitialCommit

---
+++ Aviation.WebAPI/Data/AviationContext.cs
@@ -0,0 +1,39 @@
+﻿using DomainModel;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aviation.WebAPI.Data
+{
+    public class AviationContext : DbContext
+    {
+        public AviationContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Airport>().HasData(
+                    new Airport
+                    {
+                        Id = 1,
+                        Name = "SPU",
+                        Address = "Split"
+                    },
+                    new Airport
+                    {
+                        Id = 2,
+                        Name = "ZGPleso",
+                        Address = "Zagreb"
+                    },
+                    new Airport
+                    {
+                        Id = 3,
+                        Name = "ZAD",
+                        Address = "Zadar"
+                    }
+                );
+        }
+
+        public DbSet<Airport> Airports { get; set; }
+    }
+}
---
+++ Aviation.WebAPI/Migrations/20211216135613_InitialMigrationSeedDummyData.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aviation.WebAPI.Migrations
+{
+    public partial class InitialMigrationSeedDummyData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Airports",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Airports", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Airports",
+                columns: new[] { "Id", "Address", "Name" },
+                values: new object[] { 1, "Split", "SPU" });
+
+            migrationBuilder.InsertData(
+                table: "Airports",
+                columns: new[] { "Id", "Address", "Name" },
+                values: new object[] { 2, "Zagreb", "ZGPleso" });
+
+            migrationBuilder.InsertData(
+                table: "Airports",
+                columns: new[] { "Id", "Address", "Name" },
+                values: new object[] { 3, "Zadar", "ZAD" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Airports");
+        }
+    }
+}
